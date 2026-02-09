d8ac441b3e019ab7ddf6c7128a86e4f3e825261e
Sunil Kumar Pashikanti <36616898+spashikanti@users.noreply.github.com>
2026-02-08 23:55:07 -0600

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -49,6 +49,7 @@ function renderCatalog(items) {
     grid.innerHTML = items.map(item => {
         // IT Standard: Ensure we handle the folder path correctly
         // If image_name1 is "Products_Images/xyz.jpg", the URL becomes images/Products_Images/xyz.jpg
+        console.log(item);
         const fullImagePath = `images/${item.image_name1}`;
         const displayName = currentLang === 'en' ? item.name_en : item.name_te;
 
